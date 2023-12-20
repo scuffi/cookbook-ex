0bc5484555d23ec633d6ee2ebcc3e42148d24f25
Archie <grawlixpvp@gmail.com>
2023-12-20 17:34:40 +0000

    Removed unneeded str functions in models

--- recipe_app/models.py
+++ recipe_app/models.py
@@ -1,26 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
 class Ingredient(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    recipe = models.ForeignKey('Recipe', related_name='ingredients', on_delete=models.CASCADE)
-    
-    def __str__(self) -> str:
-        return self.name
-    
+    recipe = models.ForeignKey(
+        "Recipe", related_name="ingredients", on_delete=models.CASCADE
+    )
+
+
 class Recipe(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.TextField()
-    
-    def __str__(self) -> str:
-        return self.name
-    
+
     def serialise(self):
         return {
-            'id': self.id,
-            'name': self.name,
-            'description': self.description,
-            'ingredients': [{'name': ingredient.name} for ingredient in self.ingredients.all()],
-            }
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "ingredients": [
+                {"name": ingredient.name} for ingredient in self.ingredients.all()
+            ],
+        }
