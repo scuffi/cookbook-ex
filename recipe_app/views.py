ce02bba23123fd198854dbfb6af69def35795577
Archie <grawlixpvp@gmail.com>
2023-12-20 16:34:11 +0000

    Added search query to GET and return 404 in ID lookup

--- recipe_app/views.py
+++ recipe_app/views.py
@@ -6,60 +6,76 @@ from django.http import HttpRequest
 from django.http import JsonResponse
 from django.views.generic import View
 from django.db import transaction
+from django.core.exceptions import ObjectDoesNotExist
 
 # Not stable for production, but makes testing easier
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
 from .models import Recipe, Ingredient
+from django.http import Http404
 
 
 # Create your views here.
-@method_decorator(csrf_exempt, name='dispatch')
+@method_decorator(csrf_exempt, name="dispatch")
 class RecipeView(View):
-    
     def _handle_get_by_id(self, id: int) -> Recipe:
-        return Recipe.objects.get(id=id)
-    
-    def _handle_get_all(self) -> Generator[dict, None, None]:
-        return (recipe.serialise() for recipe in Recipe.objects.all())
-    
+        try:
+            return Recipe.objects.get(id=id)
+        except Recipe.DoesNotExist:
+            raise Http404("Recipe does not exist")
+
+    def _handle_get_all(self, query: str) -> Generator[dict, None, None]:
+        if query is None:
+            return (recipe.serialise() for recipe in Recipe.objects.all())
+        return (
+            recipe.serialise()
+            for recipe in Recipe.objects.filter(name__icontains=query)
+        )
+
     def get(self, request: HttpRequest, id: Optional[int] = None) -> JsonResponse:
         if id is None:
-            return JsonResponse(list(self._handle_get_all()), safe=False)
-        
+            return JsonResponse(
+                list(self._handle_get_all(query=request.GET.get("name", None))),
+                safe=False,
+            )
+
         return JsonResponse(self._handle_get_by_id(id).serialise(), safe=False)
-        
+
     @transaction.atomic
     def post(self, request: HttpRequest) -> JsonResponse:
-        body = json.loads(request.body.decode('utf-8'))
-        
-        created_recipe = Recipe.objects.create(name=body['name'], description=body['description'])
+        body = json.loads(request.body.decode("utf-8"))
 
-        for ingredient_data in body['ingredients']:
-            Ingredient.objects.create(name=ingredient_data['name'], recipe=created_recipe)
+        created_recipe = Recipe.objects.create(
+            name=body["name"], description=body["description"]
+        )
+
+        for ingredient_data in body["ingredients"]:
+            Ingredient.objects.create(
+                name=ingredient_data["name"], recipe=created_recipe
+            )
 
         return JsonResponse(created_recipe.serialise())
-        
+
     def patch(self, request: HttpRequest, id: int) -> JsonResponse:
         recipe = self._handle_get_by_id(id)
-        body = json.loads(request.body.decode('utf-8'))
-        
-        if 'name' in body:
-            recipe.name = body['name']
-            
-        if 'description' in body:
-            recipe.description = body['description']
-            
-        if 'ingredients' in body:
+        body = json.loads(request.body.decode("utf-8"))
+
+        if "name" in body:
+            recipe.name = body["name"]
+
+        if "description" in body:
+            recipe.description = body["description"]
+
+        if "ingredients" in body:
             recipe.ingredients.all().delete()
-            for ingredient_data in body['ingredients']:
-                Ingredient.objects.create(name=ingredient_data['name'], recipe=recipe)
-                
+            for ingredient_data in body["ingredients"]:
+                Ingredient.objects.create(name=ingredient_data["name"], recipe=recipe)
+
         recipe.save()
-        
+
         return JsonResponse(recipe.serialise())
-        
+
     def delete(self, request: HttpRequest, id: int) -> JsonResponse:
         recipe = self._handle_get_by_id(id)
         recipe.delete()
