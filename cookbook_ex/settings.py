12182511d2ee745f057da318a83f1543d40beb82
Archie <grawlixpvp@gmail.com>
2023-12-20 12:38:16 +0000

    Add 'RecipeAppConfig' to INSTALLED_APPS

--- cookbook_ex/settings.py
+++ cookbook_ex/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cookbook_ex.recipe_app.apps.RecipeAppConfig'
 ]
 
 MIDDLEWARE = [
