d6f744ddff5e480e864bf8427824c394ec4bd279
Archie <grawlixpvp@gmail.com>
2023-12-21 11:29:18 +0000

    Added docstrings to RecipeView

--- recipe_app/views.py
+++ recipe_app/views.py
@@ -15,16 +15,56 @@ from django.views.decorators.csrf import csrf_exempt
 from .models import Recipe, Ingredient
 
 
-# Create your views here.
+# Make the view exempt from CSRF checks
+# ! Not secure, just used for testing purposes
 @method_decorator(csrf_exempt, name="dispatch")
 class RecipeView(View):
+    """
+    RecipeView is a view in a Django application that handles GET, POST, PATCH, and DELETE requests.
+
+    It acts as a CRUD API to interact with Recipes and their associated Ingredients.
+
+    Attributes:
+        recipes (list): The `recipes` attribute is a list of dictionaries. Each dictionary represents a
+    recipe object. Each recipe object has a `name`, `description`, and `ingredients` key. The `name` and
+    `description` keys have string values. The `ingredients` key has a list of dictionaries as its value.
+    Each dictionary in the list represents an ingredient object. Each ingredient object has a `name` key
+    with a string value.
+    """
+
     def _handle_get_by_id(self, id: int) -> Recipe:
+        """
+        The function `_handle_get_by_id` retrieves a `Recipe` object from the database based on its ID, and
+        raises a `Http404` exception if the recipe does not exist.
+
+        Args:
+          id (int): The `id` parameter is an integer that represents the unique identifier of a recipe. It
+        is used to retrieve a specific recipe from the database.
+
+        Returns:
+          a Recipe object.
+        """
         try:
             return Recipe.objects.get(id=id)
         except Recipe.DoesNotExist:
             raise Http404("Recipe does not exist")
 
     def _handle_get_all(self, query: str) -> Generator[dict, None, None]:
+        """
+        The function `_handle_get_all` returns a generator that yields serialized recipe objects based on a
+        query parameter.
+
+        Args:
+          query (str): The `query` parameter is a string that represents the search query. It is used to
+        filter the recipes based on their name. If the `query` is `None`, it means that all recipes should
+        be returned without any filtering.
+
+        Returns:
+          a generator that yields dictionaries. The dictionaries represent serialized recipe objects. If the
+        query parameter is None, the generator will yield serialized representations of all Recipe objects
+        in the database. If the query parameter is not None, the generator will yield serialized
+        representations of Recipe objects that have a name containing the query string (case-insensitive).
+        """
         if query is None:
             return (recipe.serialise() for recipe in Recipe.objects.all())
         return (
@@ -33,6 +73,24 @@ class RecipeView(View):
         )
 
     def get(self, request: HttpRequest, id: Optional[int] = None) -> JsonResponse:
+        """
+        Function that handles GET requests, either returning all objects or a
+        specific object based on the provided ID.
+
+        Args:
+          request (HttpRequest): The `request` parameter is an instance of the `HttpRequest` class, which
+        represents an HTTP request made by a client to the server. It contains information about the
+        request, such as the HTTP method (GET, POST, etc.), headers, and query parameters.
+          id (Optional[int]): The `id` parameter is an optional integer that represents the unique
+        identifier of an object. If an `id` is provided, the function will retrieve the object with that
+        specific `id`. If `id` is not provided, the function will retrieve all objects that match the query
+        parameter `name`
+
+        Returns:
+          The code is returning a JsonResponse object. If no `id` is passed, it returns a
+        JsonResponse containing a list of all recipes. If the `id`parameter is passed, it
+        returns a JsonResponse containing a single Recipe object specified by `id`.
+        """
         if id is None:
             return JsonResponse(
                 list(self._handle_get_all(query=request.GET.get("name", None))),
@@ -43,6 +101,18 @@ class RecipeView(View):
 
     @transaction.atomic
     def post(self, request: HttpRequest) -> JsonResponse:
+        """
+        Function that handles POST requests, for creating a new recipe and its associated ingredients based
+        on the request body.
+
+        Args:
+          request (HttpRequest): The `request` parameter is an instance of the `HttpRequest` class, which
+        represents an HTTP request made to the server. It contains information about the request, such as
+        the HTTP method (GET, POST, etc.), headers, and body.
+
+        Returns:
+          JsonResponse object of the created Recipe object.
+        """
         body = json.loads(request.body.decode("utf-8"))
 
         if "name" not in body or "description" not in body or "ingredients" not in body:
@@ -63,6 +133,19 @@ class RecipeView(View):
         return JsonResponse(created_recipe.serialise(), status=201)
 
     def patch(self, request: HttpRequest, id: int) -> JsonResponse:
+        """
+        Function to handle PATCH requests. The function updates a recipe object with new data provided in the request body and returns the
+        updated recipe as a JSON response.
+
+        Args:
+          request (HttpRequest): An instance of the HttpRequest class, which represents an HTTP request made
+        by a client.
+          id (int): The `id` parameter is an integer that represents the unique identifier of a recipe. It
+        is used to retrieve the recipe from the database and update its properties.
+
+        Returns:
+          a JsonResponse object with the updated Recipe object.
+        """
         recipe = self._handle_get_by_id(id)
         body = json.loads(request.body.decode("utf-8"))
 
@@ -82,6 +165,18 @@ class RecipeView(View):
         return JsonResponse(recipe.serialise())
 
     def delete(self, request: HttpRequest, id: int) -> JsonResponse:
+        """
+        Function to handle DELETE requesrs. The function deletes a recipe with the given ID and returns a
+        JSON response with a success message.
+
+        Args:
+          request (HttpRequest): HttpRequest object that represents the HTTP request made by the client.
+          id (int): The `id` parameter is an integer that represents the unique identifier of the recipe
+        that needs to be deleted.
+
+        Returns:
+          a JsonResponse object.
+        """
         recipe = self._handle_get_by_id(id)
         recipe.delete()
         return JsonResponse({"message": f"Deleted '{recipe.name}'"}, status=200)
