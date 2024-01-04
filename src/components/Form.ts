18fd25582b8f1fda30ff7c68583d89b516ae90ce
Archie <grawlixpvp@gmail.com>
2024-01-04 16:31:37 +0000

    Added more styled components

---
+++ src/components/Form.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Label = styled.label`
+    display: block;
+    margin-bottom: 0.5rem;
+
+`;
+
+export const Input = styled.input`
+    font-size: 1rem;
+    font-family: inherit;
+    border: none;
+    border-radius: 8px;
+    padding: 0.5rem 0.75rem;
+    box-shadow: 0 0px 1px hsla(0, 0%, 0%, 0.2), 0 1px 2px hsla(0, 0%, 0%, 0.2);
+    background-color: white;
+    line-height: 1.5;
+    margin: 0;
+`;
