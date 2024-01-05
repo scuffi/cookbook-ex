375260f2e979706840f634828d638a7bee38ecc8
Archie <grawlixpvp@gmail.com>
2024-01-05 15:20:11 +0000

    Added IngredientForm for ingredients

---
+++ src/components/IngredientForm.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import { Button } from "./Button";
+import { Input } from "./Form";
+
+const DeleteButton = styled.button`
+    font-size: 1rem;
+    border: none;
+    padding: 0.5rem 0.75rem;
+    cursor: pointer;
+    background-color: rgba(0, 0, 0, 0);
+`;
+
+type Props = {
+    ingredients: string[],
+    onChange: (ingredients: string[]) => void,
+};
+
+export function IngredientForm({ ingredients, onChange }: Props) {
+    function addIngredient() {
+        const newIngredients = [...ingredients];
+        newIngredients.push("");
+        onChange(newIngredients);
+    }
+
+    return <div style={{paddingBottom: "1rem"}}>{
+        ingredients.map((ingredient, index) => (
+            <div key={index} style={{paddingBottom: "1rem"}}>
+                <Input type="text" value={ingredient} onChange={event => {
+                    const newIngredients = [...ingredients];
+                    newIngredients[index] = event.target.value;
+                    onChange(newIngredients);
+                }} />
+                <DeleteButton onClick={() => {
+                    const newIngredients = [...ingredients];
+                    newIngredients.splice(index, 1);
+                    onChange(newIngredients);
+                }}>🗑️</DeleteButton>
+            </div>
+        ))}
+        <Button onClick={addIngredient}>➕</Button>
+    </div>;
+}
