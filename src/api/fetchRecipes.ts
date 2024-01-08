591ba2c845a9e16ae84b4f8500b6370815654192
Archie <grawlixpvp@gmail.com>
2024-01-08 15:45:37 +0000

    Moved all API calls to seperate files

--- src/api/fetchRecipes.ts
+++ src/api/fetchRecipes.ts
@@ -1,14 +1,16 @@
 import axios from "axios";
 import toast from "react-hot-toast";
-import { Recipe } from "../models";
 
-const fetchRecipes = async (setRecipes: (recipes: Recipe[]) => void) => {
-    axios.get("http://localhost:8000/recipes")
-        .then(response => setRecipes(response.data))
-        .catch(error => {
-            toast.error("Failed to load recipes!");
-            console.error(error);
-        });
+const fetchRecipes = async () => {
+    const response = await axios.get("http://localhost:8000/recipes");
+
+    if (response.status !== 200) {
+        toast.error("Failed to fetch recipes!");
+        console.log(response);
+        return [];
+    }
+
+    return response.data;
 }
 
 export default fetchRecipes;
