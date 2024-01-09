94fbf20e75bd740336e0ed9af563246454536e9d
Archie <grawlixpvp@gmail.com>
2024-01-09 09:51:44 +0000

    Added index files and restructured imports

---
+++ src/api/index.ts
@@ -0,0 +1,7 @@
+import fetchRecipe from "./fetchRecipe";
+import fetchRecipes from "./fetchRecipes";
+import createRecipe from "./createRecipe";
+import modifyRecipe from "./modifyRecipe";
+import deleteRecipe from "./deleteRecipe";
+
+export { fetchRecipe, fetchRecipes, createRecipe, modifyRecipe, deleteRecipe }
