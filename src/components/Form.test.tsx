f9c3d49defc027f749c2a4c24d0463a7c4130cd1
Archie <grawlixpvp@gmail.com>
2024-01-09 15:16:01 +0000

    Tweaked test definitions for Form

--- src/components/Form.test.tsx
+++ src/components/Form.test.tsx
@@ -1,16 +1,8 @@
 test("loads create form when recipe not given", async () => {});
 test("loads edit form when recipe given", async () => {});
 
-test("allows user to modify a name", async () => {});
-test("rejects submition when name input is empty", async () => {});
-
-test("allows user to modify a description", async () => {});
-test("rejects submition when description input is empty", async () => {});
-
-test("allows user to modify a icon", async () => {});
-
-test("allows user to modify ingredients", async () => {});
-test("rejects submition when an ingredient field is empty", async () => {});
+test("allows user to edit a recipe", async () => {});
+test("rejects submition when input fields are empty", async () => {});
 
 test("allows user to delete recipe", async () => {});
 test("allows user to create recipe", async () => {});
